79684a26447df82c028a6eaced2607c03355216d
Mauricio-mty <104952595+Mauricio-mty@users.noreply.github.com>
2026-03-13 10:46:07 -0600

    Export router from segmento.js

--- src/rotes/segmento.js
+++ src/rotes/segmento.js
@@ -9,3 +9,5 @@ router.get("/:id",controller.getOne);
 router.post("/",controller.create);
 router.put("/:id",controller.updateSeg);
 router.delete("/:id",controller.deleteSeg);
+
+module.exports=router;
